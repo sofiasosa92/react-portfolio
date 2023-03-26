883a16c2785183d0e0c01f9fec4d7454869b893b
Sofia92 <sofiasosaperez@outlook.com>
2023-03-25 22:10:07 -0500

    im tired. last commit for the day

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -12,7 +12,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           href="#home"
           onClick={() => handlePageChange('Home')}
           className={currentPage === 'Home' ? "btn btn-no-outline btn-md btn-hover-color" : 'nav-link'}
-          style={{ borderRadius: '90px', backgroundColor: currentPage === 'Home' ? '#3e3e3e' : 'transparent', color: 'white', padding: '10px 20px' }}
+          style={{ borderRadius: '90px', backgroundColor: currentPage === 'Home' ? '#3e3e3e' : 'transparent', color: 'white', padding: '5px 20px' }}
         >
           Home
         </a>
@@ -23,7 +23,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           href="#about"
           onClick={() => handlePageChange('About')}
           className={currentPage === 'About' ? "btn btn-no-outline btn-md btn-hover-color" : 'nav-link'}
-          style={{ borderRadius: '90px', backgroundColor: currentPage === 'About' ? '#3e3e3e' : 'transparent', color: 'white', padding: '10px 20px' }}
+          style={{ borderRadius: '90px', backgroundColor: currentPage === 'About' ? '#3e3e3e' : 'transparent', color: 'white', padding: '5px 20px' }}
         >
           About
         </a>
@@ -33,7 +33,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           href="#blog"
           onClick={() => handlePageChange('Blog')}
           className={currentPage === 'Blog' ? "btn btn-no-outline btn-md btn-hover-color" : 'nav-link'}
-          style={{ borderRadius: '90px', backgroundColor: currentPage === 'Blog' ? '#3e3e3e' : 'transparent', color: 'white', padding: '10px 20px' }}
+          style={{ borderRadius: '90px', backgroundColor: currentPage === 'Blog' ? '#3e3e3e' : 'transparent', color: 'white', padding: '5px 20px' }}
         >
           Blog
         </a>
@@ -43,13 +43,15 @@ function NavTabs({ currentPage, handlePageChange }) {
           href="#contact"
           onClick={() => handlePageChange('Contact')}
           className={currentPage === 'Contact' ? "btn btn-no-outline btn-md btn-hover-color" : 'nav-link'}
-          style={{ borderRadius: '90px', backgroundColor: currentPage === 'Contact' ? '#3e3e3e' : 'transparent', color: 'white', padding: '10px 20px' }}
-        >
-          Contact
+          style={{ borderRadius: '90px', backgroundColor: currentPage === 'Contact' ? '#3e3e3e' : 'transparent', color: 'white', padding: '5px 20px' }}
+        > Contact
         </a>
       </li>
-    </ul>
+      </ul>
+    
   );
 }
 
 export default NavTabs;
+
+
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,26 +2,37 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div style={{ backgroundColor: 'white' }}>
+    <div style={{ backgroundColor: 'white', minHeight: '100vh', position: 'relative' }}>
       <div className="container">
         <div className="row">
           <div className="col-lg-6 pr-0 mr-0 pl-lg-0 pr-lg-5 mr-lg-0 mt-3 mt-lg-0 align-items-center justify-content-center align-items-lg-left justify-content-lg-left text-center text-lg-left d-flex order-1 order-lg-0 ">
             <div className="infohero">
-            <p className="infohero__p" style={{ fontSize: '35px' }}>Hello, I'm</p>
+              <p className="infohero__p" style={{ fontSize: '35px' }}>Hello, I'm</p>
               <h1 className="infohero__title" style={{ fontSize: '50px' }}>Sofia Sosa</h1>
             </div>
           </div>
 
           <div className="col-lg-6 text-center order-0 order-lg-1 mb-5 mb-lg-0">
-          <img
-  className="rounded-circle img-fluid"
-  src="./photos/sosa.jpg"
-  style={{ width: '400px', height: '400px', float: 'right', border: '3px solid black', objectFit: 'cover' }}
-  alt="sosa.jpg"
-/>
+            <img
+              className="rounded-circle img-fluid"
+              src="./photos/sosa.jpg"
+              style={{ width: '400px', height: '400px', float: 'right', border: '3px solid black', objectFit: 'cover' }}
+              alt="sosa.jpg"
+            />
           </div>
         </div>
       </div>
+
+      <footer className="footer bg-secondary text-white text-center py-3" style={{ position: 'fixed', bottom: 0, width: '100%' }}>
+  <div className="container">
+    <div className="mt-2">
+      <a href="https://github.com/sofiasosa92" target="_blank" rel="noopener noreferrer" className="text-white mx-2"><i className="fab fa-github">GitHub</i></a>
+      <a href="https://www.linkedin.com/in/sof%C3%ADa-sosa-b43044245/" target="_blank" rel="noopener noreferrer" className="text-white mx-2"><i className="fab fa-linkedin">LinkedIn</i></a>
+      <a href="https://www.instagram.com/sofiasosa" target="_blank" rel="noopener noreferrer" className="text-white mx-2"><i className="fab fa-instagram">Instagram</i></a>
+    </div>
+  </div>
+</footer>
+
     </div>
   );
 }
